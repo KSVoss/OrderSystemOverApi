b43916d3953f1bca7a22607f8d8d5d7f9f5da669
KSVoss <Kai.S.Voss@t-online.de>
2022-07-13 11:20:25 +0200

    OrderSystemWeb

---
+++ src/main/java/com/example/ordersystemoverapi/OrderSystemOverApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ordersystemoverapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSystemOverApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderSystemOverApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ordersystemoverapi/OrderSystemOverApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ordersystemoverapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSystemOverApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
